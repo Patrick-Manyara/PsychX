8b1121f20d9c53c784e4055bed9d881db70ec381
FionaWekulo <fionawekulo+1@gmail.com>
2023-10-25 22:38:30 +0300

    implemented corporate viewing used vouchers and requesting admin to see who used them

--- corporate/send_email_request.php
+++ corporate/send_email_request.php
@@ -5,9 +5,18 @@ $conn = connect();
 if (isset($_GET['id'])) {
     $id = security('id','GET');
  }
+ 
+        
 $corporate = select_rows("SELECT * FROM corporate WHERE corporate_id = '$id' ");
 $vouchers = select_rows("SELECT * FROM voucher WHERE corporate_id = '$id' AND voucher_used = 'yes'");
-
+$voucher_codes = [];
+foreach ($vouchers as $voucher) {
+    $voucher_codes[] = $voucher['voucher_code'];
+}
+$arr['vouchers'] = implode(", ", $voucher_codes);  // Join all voucher codes with a comma separator
+$arr['request_id'] = create_idd('request', 'request_id');
+$arr['corporate_id'] = $id;  
+build_sql_insert('request', $arr);
 
 
     $co  = '<p style="font-family:Poppins, sans-serif;"> ';
@@ -22,3 +31,24 @@ $vouchers = select_rows("SELECT * FROM voucher WHERE corporate_id = '$id' AND vo
     email($cemail, APP_NAME . " Corporate Voucher Use", APP_NAME, $co);
 
     header("Location: view_used_vouchers");
+
+    function create_idd($table, $id)
+    {
+        $date_today = date('Ymd');
+    
+        $table_prifix = array(
+            'request'             => 'RQT' . $date_today,
+            
+        );
+    
+        $random_str = $table_prifix[$table] . rand_str();
+    
+        $get_id     = get_ids($table, $id, $random_str);
+    
+        while ($get_id == true) {
+            $random_str = $table_prifix[$table] . rand_str();
+            $get_id     = get_ids($table, $id, $random_str);
+        }
+        return $random_str;
+    }
+    
